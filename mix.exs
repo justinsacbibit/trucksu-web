c1a0a82c7af5a744e5c772c4255f8bb0f20cddda
Justin Sacbibit <jsacbibit@gmail.com>
2016-04-07 03:42:31 -0400

    Add Ansible provisioning

--- mix.exs
+++ mix.exs
@@ -21,6 +21,6 @@ defmodule TrucksuUmbrella.Mixfile do
   # Dependencies listed here are available only for this project
   # and cannot be accessed from applications inside the apps folder
   defp deps do
-    []
+    [{:exrm, "~> 0.18.1"}]
   end
 end
