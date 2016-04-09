81ec806d2c36169928dbbd61f215aadc398c6a47
Justin Sacbibit <jsacbibit@gmail.com>
2016-04-09 17:55:04 -0400

    Update endpoint

--- lib/trucksu/endpoint.ex
+++ lib/trucksu/endpoint.ex
@@ -8,7 +8,7 @@ defmodule Trucksu.Endpoint do
   # You should set gzip to true if you are running phoenix.digest
   # when deploying your static files in production.
   plug Plug.Static,
-    at: "/osu", from: :trucksu, gzip: false,
+    at: "/", from: :trucksu, gzip: true,
     only: ~w(css fonts images js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
