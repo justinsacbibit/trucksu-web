122088e9be97b290e858800b404ca2ef5b61417d
Justin Sacbibit <jsacbibit@gmail.com>
2016-04-27 19:08:53 -0400

    Fix division by 0

--- web/helpers/accuracy.ex
+++ web/helpers/accuracy.ex
@@ -1,6 +1,10 @@
 defmodule Trucksu.Accuracy do
   alias Trucksu.Score
 
+  def from_accuracies([]) do
+    1
+  end
+
   def from_accuracies(accuracies) do
     {numerator, denominator, _} = Enum.reduce accuracies, {0, 0, 0}, fn accuracy, {numerator, denominator, index} ->
       factor = :math.pow(0.95, index)
