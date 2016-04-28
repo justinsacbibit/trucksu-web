7a44bd6013bf6d5110cc7591eeefac4fdaa6357c
Justin Sacbibit <jsacbibit@gmail.com>
2016-04-28 17:33:11 -0400

    Add hardcoded nano avatar

--- web/controllers/avatar_controller.ex
+++ web/controllers/avatar_controller.ex
@@ -2,13 +2,17 @@ defmodule Trucksu.AvatarController do
   use Trucksu.Web, :controller
 
   defp avatar_path(filename) do
-    # TODO: Figure out how not to hardcode the path
     "web/static/images/" <> filename
   end
 
-  def show(conn, _params) do
+  def show(conn, %{"user_id" => user_id}) do
     # TODO: Return the correct avatar based on params["user_id"]
-    path = avatar_path("default_avatar.jpg")
+    # TODO: Use S3 for storing the images
+    filename = case user_id do
+      "8" -> "unsaturated.jpg"
+      _ -> "default_avatar.jpg"
+    end
+    path = avatar_path(filename)
     Plug.Conn.send_file(conn, 200, path)
   end
 end
