9d312b0f078911ca6186af2b5564094594aee6f6
Justin Sacbibit <jsacbibit@gmail.com>
2016-05-05 12:59:14 -0400

    Make OsuBeatmap.max_combo optional (other game modes)

--- web/models/osu_beatmap.ex
+++ web/models/osu_beatmap.ex
@@ -51,8 +51,8 @@ defmodule Trucksu.OsuBeatmap do
     timestamps
   end
 
-  @required_fields ~w(beatmapset_id beatmap_id approved total_length hit_length version file_md5 diff_size diff_overall diff_approach diff_drain game_mode last_update artist title creator bpm source tags genre_id language_id favourite_count playcount passcount max_combo difficultyrating)
-  @optional_fields ~w(approved_date)
+  @required_fields ~w(beatmapset_id beatmap_id approved total_length hit_length version file_md5 diff_size diff_overall diff_approach diff_drain game_mode last_update artist title creator bpm source tags genre_id language_id favourite_count playcount passcount difficultyrating)
+  @optional_fields ~w(max_combo approved_date)
 
   @doc """
   Creates a changeset based on the `model` and `params`.
