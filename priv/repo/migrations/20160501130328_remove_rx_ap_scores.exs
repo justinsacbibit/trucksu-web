22fa4166013d159882de4166cc3b295b2ec7446d
Justin Sacbibit <jsacbibit@gmail.com>
2016-05-23 01:34:22 -0400

    Clean up migration

--- priv/repo/migrations/20160501130328_remove_rx_ap_scores.exs
+++ priv/repo/migrations/20160501130328_remove_rx_ap_scores.exs
@@ -5,14 +5,15 @@ defmodule Trucksu.Repo.Migrations.RemoveRxApScores do
   alias Trucksu.{Repo, Score}
 
   def change do
-    scores = Repo.all from s in Score,
-      where: fragment("mods & (?) <> 0", 128)
-        or fragment("mods & (?) <> 0", 2048)
-        or fragment("mods & (?) <> 0", 8192)
+    # Already run in prod
+    # scores = Repo.all from s in Score,
+    #   where: fragment("mods & (?) <> 0", 128)
+    #     or fragment("mods & (?) <> 0", 2048)
+    #     or fragment("mods & (?) <> 0", 8192)
 
-    for score <- scores do
-      Logger.warn "Deleting score: #{inspect score}"
-      Repo.delete! score
-    end
+    # for score <- scores do
+    #   Logger.warn "Deleting score: #{inspect score}"
+    #   Repo.delete! score
+    # end
   end
 end
