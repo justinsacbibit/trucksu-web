b25cd40adae947607460c76b46b6d726feb5484c
Justin Sacbibit <jsacbibit@gmail.com>
2016-06-18 14:08:19 -0400

    Add User.find_multiaccounts_by_ip

--- web/models/user.ex
+++ web/models/user.ex
@@ -1,25 +1,27 @@
 defmodule Trucksu.User do
   use Trucksu.Web, :model
-  alias Trucksu.{
-    Friendship,
-    Score,
-    UserStats,
-  }
+  alias Trucksu.Repo
 
-  @derive {Poison.Encoder, only: [:id, :username, :email]}
+  @derive {Poison.Encoder, only: [
+    :id,
+    :username,
+    :email,
+  ]}
 
   schema "users" do
     field :username, :string
     field :email, :string
     field :encrypted_password, :string
     field :password, :string, virtual: true
-    has_many :stats, UserStats
-    has_many :scores, Score
+    has_many :stats, Trucksu.UserStats
+    has_many :scores, Trucksu.Score
     field :country, :string
     field :banned, :boolean, default: false
 
-    has_many :friendships, Friendship, foreign_key: :requester_id
+    has_many :friendships, Trucksu.Friendship, foreign_key: :requester_id
     has_many :friends, through: [:friendships, :receiver]
+
+    has_many :known_ips, Trucksu.KnownIp
 
     timestamps
   end
@@ -33,6 +35,35 @@ defmodule Trucksu.User do
   def by_username(username) do
     from u in __MODULE__,
       where: fragment("lower(?)", u.username) == fragment("lower(?)", ^username)
+  end
+
+  @doc """
+  Builds a Map of users who seem to be multiaccounts for the given user, by IP address.
+  The map returned maps user ids to user structs.
+
+  Does a depth-first search on a graph where a user is a node and an edge exists between
+  users if they share at least one IP address.
+
+  """
+  def find_multiaccounts_by_ip(%__MODULE__{id: id} = user, accounts_so_far \\ %{}) do
+    if Map.has_key?(accounts_so_far, id) do
+      # base case
+      accounts_so_far
+    else
+      user = Repo.preload user, :known_ips
+      accounts_so_far = Map.put(accounts_so_far, id, user)
+
+      Enum.reduce(user.known_ips, accounts_so_far, fn(known_ip, accounts_so_far) ->
+        known_ip_address = known_ip.ip_address
+        users_with_same_ip = Repo.all from u in __MODULE__,
+          join: ip in assoc(u, :known_ips),
+          where: ip.ip_address == ^known_ip_address
+
+        Enum.reduce(users_with_same_ip, accounts_so_far, fn(user_with_same_ip, accounts_so_far) ->
+          Map.merge(accounts_so_far, find_multiaccounts_by_ip(user_with_same_ip, accounts_so_far))
+        end)
+      end)
+    end
   end
 
   @doc """
