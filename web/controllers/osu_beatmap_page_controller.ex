3b4577b615b5337b094b556db72f1842caef80ac
Justin Sacbibit <jsacbibit@gmail.com>
2016-06-20 22:22:18 -0400

    Redirect osu beatmap page to trucksu

--- web/controllers/osu_beatmap_page_controller.ex
+++ web/controllers/osu_beatmap_page_controller.ex
@@ -2,7 +2,8 @@ defmodule Trucksu.OsuBeatmapPageController do
   use Trucksu.Web, :controller
 
   def show_beatmap(conn, %{"beatmap_id" => beatmap_id}) do
-    redirect conn, external: "http://new.ppy.sh/b/#{beatmap_id}"
+    website_url = Application.get_env(:trucksu, :website_url)
+    redirect conn, external: "#{website_url}/beatmaps/#{beatmap_id}"
   end
 
   def show_beatmapset(conn, %{"beatmapset_id" => beatmapset_id}) do
