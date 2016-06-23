932ee8232ba4fcf3e31c43637c90e7ddd8f77750
Justin Sacbibit <jsacbibit@gmail.com>
2016-06-22 20:40:16 -0400

    Include access point in multis api response (#98)

--- web/models/user.ex
+++ web/models/user.ex
@@ -22,6 +22,7 @@ defmodule Trucksu.User do
     has_many :friends, through: [:friendships, :receiver]
 
     has_many :known_ips, Trucksu.KnownIp
+    has_many :access_points, Trucksu.OsuUserAccessPoint
 
     timestamps
   end
@@ -35,6 +36,36 @@ defmodule Trucksu.User do
   def by_username(username) do
     from u in __MODULE__,
       where: fragment("lower(?)", u.username) == fragment("lower(?)", ^username)
+  end
+
+  @doc """
+  Builds a Map of users who seem to be multiaccounts for the given user, by access point.
+  The map returned maps user ids to user structs.
+
+  Does a depth-first search on a graph where a user is a node and an edge exists between
+  users if they share at least one access point.
+
+  """
+  def find_multiaccounts_by_access_point(%__MODULE__{id: id} = user, accounts_so_far \\ %{}) do
+    if Map.has_key?(accounts_so_far, id) do
+      # base case
+      accounts_so_far
+    else
+      user = Repo.preload user, :access_points
+      accounts_so_far = Map.put(accounts_so_far, id, user)
+
+      Enum.reduce(user.access_points, accounts_so_far, fn(access_point, accounts_so_far) ->
+        %{unique_md5: unique_md5, disk_md5: disk_md5} = access_point
+        users_with_same_access_point = Repo.all from u in __MODULE__,
+          join: ap in assoc(u, :access_points),
+          where: ap.unique_md5 == ^unique_md5
+            or ((not is_nil(fragment("(?)::bytea", ^disk_md5)) and not is_nil(ap.disk_md5)) and ap.disk_md5 == ^disk_md5)
+
+        Enum.reduce(users_with_same_access_point, accounts_so_far, fn(user_with_same_access_point, accounts_so_far) ->
+          Map.merge(accounts_so_far, find_multiaccounts_by_access_point(user_with_same_access_point, accounts_so_far))
+        end)
+      end)
+    end
   end
 
   @doc """
