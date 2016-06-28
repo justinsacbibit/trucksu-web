f5c18e5d99e1a3f74d97f797d40c1548656663d9
Justin Sacbibit <jsacbibit@gmail.com>
2016-06-28 08:26:37 -0400

    Pass through direct query

--- web/controllers/osu_direct_controller.ex
+++ web/controllers/osu_direct_controller.ex
@@ -31,28 +31,46 @@ defmodule Trucksu.OsuDirectController do
   end
 
   def direct_index(conn, params) do
-    beatmapsets = Repo.all from obs in OsuBeatmapset,
-      select: obs
-
-    data = Enum.reduce(beatmapsets, "", fn(beatmapset, data) ->
-      data <> format_direct(beatmapset)
-    end)
-
-    data = "#{length beatmapsets}\n" <> data
-
-    conn
-    |> html(data)
+    osu_username = Application.get_env(:trucksu, :osu_username)
+    osu_password_md5 = Application.get_env(:trucksu, :osu_password_md5)
+    params = params
+    |> Map.put("u", osu_username)
+    |> Map.put("h", osu_password_md5)
+    case HTTPoison.get("https://osu.ppy.sh/web/osu-search.php", [], follow_redirect: true, params: Enum.to_list(params)) do
+      {:ok, %HTTPoison.Response{body: body}} ->
+        conn
+        |> html(body)
+      {:error, error} ->
+        Logger.error "Error getting osu!direct listing: #{inspect error}"
+        conn
+        |> html("")
+    end
   end
 
-  defp format_direct(beatmapset) do
-    has_video = 0 # 0 = no video, 1 = has video
-    artist = String.replace(beatmapset.artist, "|", "I")
-    title = String.replace(beatmapset.title, "|", "I")
-    thread_id = beatmapset.id
-    # missing difficulty names
-    "#{beatmapset.id}.osz|#{artist}|#{title}|#{beatmapset.creator}|#{beatmapset.approved}|10.00000|1|#{beatmapset.id}|#{thread_id}|#{has_video}|0|0|0|diff\n"
-    "#{beatmapset.id}.osz|#{artist}|#{title}|#{beatmapset.creator}|#{1}|10.00000|1|#{beatmapset.id}|#{thread_id}|#{has_video}|0|0||Diff@0|\n"
-  end
+  # def direct_index(conn, params) do
+  #   beatmapsets = Repo.all from obs in OsuBeatmapset,
+  #     join: ob in assoc(obs, :beatmaps),
+  #     preload: [beatmaps: ob]
+
+  #   data = Enum.reduce(beatmapsets, "", fn(beatmapset, data) ->
+  #     data <> format_direct(beatmapset)
+  #   end)
+
+  #   data = "#{length beatmapsets}\n" <> data
+
+  #   conn
+  #   |> html(data)
+  # end
+
+  # defp format_direct(beatmapset) do
+  #   has_video = 0 # 0 = no video, 1 = has video
+  #   artist = String.replace(beatmapset.artist, "|", "I")
+  #   title = String.replace(beatmapset.title, "|", "I")
+  #   thread_id = Enum.at(beatmapset.beatmaps, 0) |> Map.get(:id)
+  #   # missing difficulty names
+  #   diffs = Enum.join(Enum.map(beatmapset.beatmaps, &Map.get(&1, :version)), ",")
+  #   "#{beatmapset.id}.osz|#{artist}|#{title}|#{beatmapset.creator}|#{1}|10.00000|1|#{beatmapset.id}|#{thread_id}|#{has_video}|0|0||#{diffs}|\n"
+  # end
 
   defp stop(conn, status_code) do
     conn
