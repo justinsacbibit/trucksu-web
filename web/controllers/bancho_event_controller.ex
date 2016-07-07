4069a68fb3fa60aa320b2c4479b185d058589b88
Justin Sacbibit <jsacbibit@gmail.com>
2016-07-07 02:55:30 -0400

    Broadcast realtime to userpage

--- web/controllers/bancho_event_controller.ex
+++ web/controllers/bancho_event_controller.ex
@@ -9,25 +9,28 @@ defmodule Trucksu.BanchoEventController do
     Map.put(user, "rank", Repo.one(UserStats.get_rank(user["id"], 0)))
   end
 
-  def create(conn, %{"type" => "user_online" = type, "user" => user}) do
+  defp broadcast_user(user, event_type) do
     user = put_rank(user)
-    Trucksu.Endpoint.broadcast! "users", type, %{user: user}
+    Trucksu.Endpoint.broadcast! "users", event_type, %{user: user}
+    Trucksu.Endpoint.broadcast! "users:#{user["id"]}", event_type, %{user: user}
+  end
+
+  def create(conn, %{"type" => "user_online" = type, "user" => user}) do
+    broadcast_user(user, type)
 
     conn
     |> json(%{"ok" => true})
   end
 
   def create(conn, %{"type" => "user_offline" = type, "user" => user}) do
-    user = put_rank(user)
-    Trucksu.Endpoint.broadcast! "users", type, %{user: user}
+    broadcast_user(user, type)
 
     conn
     |> json(%{"ok" => true})
   end
 
   def create(conn, %{"type" => "user_change_action" = type, "user" => user}) do
-    user = put_rank(user)
-    Trucksu.Endpoint.broadcast! "users", type, %{user: user}
+    broadcast_user(user, type)
 
     conn
     |> json(%{"ok" => true})
