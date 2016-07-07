6f1757163f6ed4dc732c4f887bba3b9ad25df8a9
Justin Sacbibit <jsacbibit@gmail.com>
2016-07-06 22:00:26 -0400

    Uncomment realtime

--- web/channels/user_channel.ex
+++ web/channels/user_channel.ex
@@ -7,23 +7,23 @@ defmodule Trucksu.UserChannel do
     {:ok, socket}
   end
 
-  # def handle_in("get:users", _, socket) do
-  #   bancho_url = Application.get_env(:trucksu, :bancho_url)
-  #   case HTTPoison.get(bancho_url <> "/api/v1/users") do
-  #     {:ok, %HTTPoison.Response{body: user_actions}} ->
-  #       user_actions = Poison.decode! user_actions
-  #       users = for user_action <- user_actions do
-  #         user = Repo.get! User, user_action["id"]
-  #         rank = Repo.one(UserStats.get_rank(user_action["id"], 0))
-  #         user_action
-  #         |> Map.put("username", user.username)
-  #         |> Map.put("rank", rank)
-  #       end
-  #       {:reply, {:ok, %{users: users}}, socket}
-  #     {:error, error} ->
-  #       Logger.error "Error getting online users from bancho: #{inspect error}"
-  #       {:reply, {:error, %{}}, socket}
-  #   end
-  # end
+  def handle_in("get:users", _, socket) do
+    bancho_url = Application.get_env(:trucksu, :bancho_url)
+    case HTTPoison.get(bancho_url <> "/api/v1/users") do
+      {:ok, %HTTPoison.Response{body: user_actions}} ->
+        user_actions = Poison.decode! user_actions
+        users = for user_action <- user_actions do
+          user = Repo.get! User, user_action["id"]
+          rank = Repo.one(UserStats.get_rank(user_action["id"], 0))
+          user_action
+          |> Map.put("username", user.username)
+          |> Map.put("rank", rank)
+        end
+        {:reply, {:ok, %{users: users}}, socket}
+      {:error, error} ->
+        Logger.error "Error getting online users from bancho: #{inspect error}"
+        {:reply, {:error, %{}}, socket}
+    end
+  end
 end
 
--- web/controllers/bancho_event_controller.ex
+++ web/controllers/bancho_event_controller.ex
@@ -5,39 +5,39 @@ defmodule Trucksu.BanchoEventController do
 
   # TODO: Check server cookie
 
-  # defp put_rank(user) do
-  #   Map.put(user, "rank", Repo.one(UserStats.get_rank(user["id"], 0)))
-  # end
+  defp put_rank(user) do
+    Map.put(user, "rank", Repo.one(UserStats.get_rank(user["id"], 0)))
+  end
 
-  # def create(conn, %{"type" => "user_online" = type, "user" => user}) do
-  #   user = put_rank(user)
-  #   Trucksu.Endpoint.broadcast! "users", type, %{user: user}
+  def create(conn, %{"type" => "user_online" = type, "user" => user}) do
+    user = put_rank(user)
+    Trucksu.Endpoint.broadcast! "users", type, %{user: user}
 
-  #   conn
-  #   |> json(%{"ok" => true})
-  # end
+    conn
+    |> json(%{"ok" => true})
+  end
 
-  # def create(conn, %{"type" => "user_offline" = type, "user" => user}) do
-  #   user = put_rank(user)
-  #   Trucksu.Endpoint.broadcast! "users", type, %{user: user}
+  def create(conn, %{"type" => "user_offline" = type, "user" => user}) do
+    user = put_rank(user)
+    Trucksu.Endpoint.broadcast! "users", type, %{user: user}
 
-  #   conn
-  #   |> json(%{"ok" => true})
-  # end
+    conn
+    |> json(%{"ok" => true})
+  end
 
-  # def create(conn, %{"type" => "user_change_action" = type, "user" => user}) do
-  #   user = put_rank(user)
-  #   Trucksu.Endpoint.broadcast! "users", type, %{user: user}
+  def create(conn, %{"type" => "user_change_action" = type, "user" => user}) do
+    user = put_rank(user)
+    Trucksu.Endpoint.broadcast! "users", type, %{user: user}
 
-  #   conn
-  #   |> json(%{"ok" => true})
-  # end
+    conn
+    |> json(%{"ok" => true})
+  end
 
-  # def create(conn, %{"type" => type}) do
-  #   Logger.error "Got unknown type from bancho: #{inspect type}"
+  def create(conn, %{"type" => type}) do
+    Logger.error "Got unknown type from bancho: #{inspect type}"
 
-  #   conn
-  #   |> json(%{"ok" => true})
-  # end
+    conn
+    |> json(%{"ok" => true})
+  end
 end
 
