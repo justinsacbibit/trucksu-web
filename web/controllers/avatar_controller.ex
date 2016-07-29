f765ef54a4d2f415d6ef9dfa09d9c184844501bc
Justin Sacbibit <jsacbibit@gmail.com>
2016-07-29 07:47:20 -0400

    Cache avatars

--- web/controllers/avatar_controller.ex
+++ web/controllers/avatar_controller.ex
@@ -1,25 +1,48 @@
 defmodule Trucksu.AvatarController do
   use Trucksu.Web, :controller
 
+  @default_path "web/static/images/default_avatar.jpg"
+  @avatar_file_bucket Application.get_env(:trucksu, :avatar_file_bucket)
+
+  def show(conn, %{"user_id" => "-1"}) do
+    Plug.Conn.send_file(conn, 200, @default_path)
+  end
   def show(conn, %{"user_id" => user_id}) do
-    default_path = "web/static/images/default_avatar.jpg"
-    case user_id do
-      "-1" ->
-        Plug.Conn.send_file(conn, 200, default_path)
-      _ ->
-        bucket = Application.get_env(:trucksu, :avatar_file_bucket)
-        case ExAws.S3.get_object(bucket, user_id) do
-          {:error, {:http_error, 404, _}} ->
+    case ExAws.S3.get_object(@avatar_file_bucket, user_id) do
+      {:error, {:http_error, 404, _}} ->
+        conn
+        |> put_cache_headers("default-avatar")
+        |> Plug.Conn.send_file(200, @default_path)
 
-            Plug.Conn.send_file(conn, 200, default_path)
-
-          {:ok, %{body: avatar_file_content}} ->
+      {:ok, %{body: avatar_file_content, headers: headers}} ->
+        case find_etag(headers) do
+          nil ->
             conn
-            |> Plug.Conn.put_resp_header("content-type", "image/jpeg")
-            |> Plug.Conn.put_resp_header("content-transfer-encoding", "binary")
-            |> Plug.Conn.send_resp(200, avatar_file_content)
+          etag ->
+            conn
+            |> put_cache_headers(etag)
         end
+
+        conn
+        |> Plug.Conn.put_resp_header("content-type", "image/jpeg")
+        |> Plug.Conn.put_resp_header("content-transfer-encoding", "binary")
+        |> Plug.Conn.send_resp(200, avatar_file_content)
     end
+  end
+
+  defp find_etag(headers) do
+    case Enum.find(headers, fn({key, _value}) -> key == "ETag" end) do
+      {_key, etag} ->
+        etag
+      _ ->
+        nil
+    end
+  end
+
+  defp put_cache_headers(conn, etag) do
+    conn
+    |> Plug.Conn.put_resp_header("cache-control", "public, max-age=31536000")
+    |> Plug.Conn.put_resp_header("ETag", etag)
   end
 end
 
